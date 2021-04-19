f79f1ab87e02f85653c7a04e24f2b05045808624
Aaron Bies <48803074+slerpyyy@users.noreply.github.com>
2021-04-19 23:50:26 +0200

    Starting over, now with imgui, sdl2 and gl

---
+++ README.md
@@ -0,0 +1,39 @@
+# Sh4derJockey
+*A custom VJ tool written by sp4ghet and slerpy*
+
+![code-quality](https://img.shields.io/badge/code%20quality-jank-red)
+![works-on](https://img.shields.io/badge/works%20on-my%20mashine%E2%84%A2-orange)
+
+## Wishlist
+
+A non-exhaustive list of things we want our tool to be able to do, vaguely ordered by priority, from high to low:
+
+- [ ] fullscreen fragment shaders
+- [ ] live shader reloading
+- [ ] dynamic / configurable render pipeline
+- [ ] MIDI control
+- [ ] audio reactive / FFT
+- [ ] tap-to-sync / BPM calculator
+- [ ] code view
+- [ ] vertex shaders
+- [ ] compute shaders
+- [ ] curve editor
+- [ ] color palette
+- [ ] preformance profiler
+
+## Setup
+
+Before building this project, make sure you have the **SDL2 development library** installed. You can find instructions on how to do that on the [rust-sdl2 repo](https://github.com/Rust-SDL2/rust-sdl2#requirements).
+
+Once that's done, the rest should be muscle memory:
+```sh
+# clone the repo
+git clone https://github.com/slerpyyy/sh4der-jockey.git
+cd sh4der-jockey
+
+# build and run
+cargo run
+
+# install so you can run it from anywhere
+cargo install --path .
+```
--- src/main.rs
+++ src/main.rs
@@ -1,194 +1,237 @@
-#![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
-#![allow(dead_code, unused_imports)]
+//#![allow(dead_code)]
 
-#[macro_use]
-extern crate glium;
+extern crate gl;
+extern crate imgui;
+extern crate imgui_opengl_renderer;
+extern crate imgui_sdl2;
+extern crate sdl2;
 
-use glium::{
-    glutin,
-    index::{NoIndices, PrimitiveType},
-    texture::Texture2d,
-    uniforms::{MagnifySamplerFilter, UniformValue},
-    Display, Frame, Surface, VertexBuffer,
-};
+use gl::types::*;
+use std::ffi::CString;
+use std::mem;
+use std::ptr;
+use std::str;
+use std::time::Instant;
 
-use glutin::{
-    event::{Event, StartCause, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-    ContextBuilder,
-};
+const VS_SRC: &'static str = "
+#version 150
+in vec2 position;
+void main() {
+    gl_Position = vec4(position, 0.0, 1.0);
+}";
 
-use getopts::Options;
-use std::{
-    collections::HashMap,
-    fs::File,
-    time::{Duration, Instant},
-};
+const FS_SRC: &'static str = "
+#version 150
+out vec4 out_color;
+uniform vec3 R;
+uniform float time;
+void main() {
+    out_color = vec4(gl_FragCoord.xy / R.xy, 0.5 + 0.5 * sin(R.z), 1.0);
+}";
 
-mod pipeline;
-use pipeline::Pipeline;
+const FULLSCREEN_RECT: [GLfloat; 12] = [
+    -1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, -1.0,
+];
 
-mod helper;
-use helper::*;
+pub fn compile_shader(src: &str, ty: GLenum) -> GLuint {
+    unsafe {
+        let shader = gl::CreateShader(ty);
 
-#[derive(Clone, Copy)]
-struct Vertex {
-    position: [f32; 2],
-}
+        // Attempt to compile the shader
+        let c_str = CString::new(src.as_bytes()).unwrap();
+        gl::ShaderSource(shader, 1, &c_str.as_ptr(), ptr::null());
+        gl::CompileShader(shader);
 
-impl Vertex {
-    fn new(x: f32, y: f32) -> Self {
-        Vertex { position: [x, y] }
+        // Get the compile status
+        let mut status = gl::FALSE as GLint;
+        gl::GetShaderiv(shader, gl::COMPILE_STATUS, &mut status);
+
+        // Fail on error
+        if status != (gl::TRUE as GLint) {
+            let mut len = 0;
+            gl::GetShaderiv(shader, gl::INFO_LOG_LENGTH, &mut len);
+            let mut buf = Vec::with_capacity(len as usize);
+            buf.set_len((len as usize) - 1); // subtract 1 to skip the trailing null character
+            gl::GetShaderInfoLog(
+                shader,
+                len,
+                ptr::null_mut(),
+                buf.as_mut_ptr() as *mut GLchar,
+            );
+            panic!(
+                "{}",
+                str::from_utf8(&buf).expect("ShaderInfoLog not valid utf8")
+            );
+        }
+        shader
     }
 }
 
-implement_vertex!(Vertex, position);
+pub fn link_program(vs: GLuint, fs: GLuint) -> GLuint {
+    unsafe {
+        let program = gl::CreateProgram();
 
-fn print_usage(name: &str, opts: &Options) {
-    println!("Usage: {} [option]\n", name);
-    print!("A custom VJ tool written by sp4ghet and slerpy.");
-    println!("{}", opts.usage(""));
+        gl::AttachShader(program, vs);
+        gl::AttachShader(program, fs);
+        gl::LinkProgram(program);
+
+        // Get the link status
+        let mut status = gl::FALSE as GLint;
+        gl::GetProgramiv(program, gl::LINK_STATUS, &mut status);
+
+        // Fail on error
+        if status != (gl::TRUE as GLint) {
+            let mut len: GLint = 0;
+            gl::GetProgramiv(program, gl::INFO_LOG_LENGTH, &mut len);
+            let mut buf = Vec::with_capacity(len as usize);
+            buf.set_len((len as usize) - 1); // subtract 1 to skip the trailing null character
+            gl::GetProgramInfoLog(
+                program,
+                len,
+                ptr::null_mut(),
+                buf.as_mut_ptr() as *mut GLchar,
+            );
+            panic!(
+                "{}",
+                str::from_utf8(&buf).expect("ProgramInfoLog not valid utf8")
+            );
+        }
+
+        program
+    }
 }
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let bin_name = args.get(0).unwrap();
+    let sdl_context = sdl2::init().unwrap();
+    let video = sdl_context.video().unwrap();
 
-    let mut opts = Options::new();
-    opts.optflag("h", "help", "print this help message");
-    opts.optflag("i", "init", "set up a simple example project");
-
-    let matches = opts.parse(&args[1..]).expect("failed to parse args");
-    if matches.opt_present("h") {
-        print_usage(bin_name, &opts);
-        return;
+    {
+        let gl_attr = video.gl_attr();
+        gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+        gl_attr.set_context_version(3, 0);
     }
 
-    if matches.opt_present("i") {
-        std::fs::write("./pipeline.json", include_str!("defaults/pipeline.json")).unwrap();
-        std::fs::write("./fs.glsl", include_str!("defaults/fs.glsl")).unwrap();
-        std::fs::write("./vs.glsl", include_str!("defaults/vs.glsl")).unwrap();
-        return;
-    }
+    let window = video
+        .window("rust-imgui-sdl2 demo", 1080, 720)
+        .position_centered()
+        .resizable()
+        .opengl()
+        .allow_highdpi()
+        .build()
+        .unwrap();
 
-    let dir_name = std::env::current_dir()
-        .expect("could not resolve cwd")
-        .to_str()
-        .expect("could not convert path to string")
-        .to_string();
+    let _gl_context = window
+        .gl_create_context()
+        .expect("Couldn't create GL context");
 
-    println!("Using folder: \"{}\"", dir_name);
+    gl::load_with(|s| video.gl_get_proc_address(s) as _);
 
-    let pipeline_file =
-        File::open(format!("{}/pipeline.json", dir_name)).expect("could not open pipeline file");
-    let pipeline_json: serde_json::Value =
-        serde_json::from_reader(pipeline_file).expect("could not parse pipeline");
+    let mut imgui = imgui::Context::create();
+    imgui.set_ini_filename(None);
 
-    println!("Json parsed: \"{:#?}\"", pipeline_json);
-
-    let event_loop = EventLoop::new();
-    let wb = WindowBuilder::new().with_title("VJ tool stuff idk");
-    let cb = ContextBuilder::new();
-    let display = Display::new(wb, cb, &event_loop).unwrap();
-
-    let pipeline = Pipeline::from_json(pipeline_json, &display);
-    println!("{:#?}", pipeline);
-
-    // create fullscreen rect
-    let v1 = Vertex::new(-1.0, -1.0);
-    let v2 = Vertex::new(-1.0, 1.0);
-    let v3 = Vertex::new(1.0, 1.0);
-    let v4 = Vertex::new(1.0, -1.0);
-
-    let shape = vec![v1, v2, v3, v3, v4, v1];
-    let vertex_buffer = VertexBuffer::new(&display, &shape).unwrap();
-    let indices = NoIndices(PrimitiveType::TrianglesList);
-
-    // get screen size
-    let (mut width, mut height) = display.get_framebuffer_dimensions();
-
-    // init buffers
-    let mut textures = HashMap::new();
-    for stage in pipeline.stages.iter() {
-        if let Some(tex_name) = stage.target.clone() {
-            textures.insert(
-                tex_name,
-                Texture2d::empty(&display, width, height).unwrap(),
-            );
-        }
-    }
-
-    // make dummy texture
-    let dummy = Texture2d::empty(&display, 1, 1).unwrap();
-
-    // start event loop
+    let mut imgui_sdl2 = imgui_sdl2::ImguiSdl2::new(&mut imgui, &window);
+    let renderer =
+        imgui_opengl_renderer::Renderer::new(&mut imgui, |s| video.gl_get_proc_address(s) as _);
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut last_frame = Instant::now();
     let start_time = Instant::now();
-    event_loop.run(move |event, _, control_flow| {
-        // react to event
-        match event {
-            Event::WindowEvent { event, .. } => match event {
-                WindowEvent::CloseRequested => {
-                    *control_flow = ControlFlow::Exit;
-                    return;
-                }
-                WindowEvent::Resized(size) => {
-                    width = size.width;
-                    height = size.height;
-                }
-                _ => return,
-            },
-            Event::NewEvents(cause) => match cause {
-                StartCause::ResumeTimeReached { .. } => (),
-                StartCause::Init => (),
-                _ => return,
-            },
-            _ => return,
-        }
 
-        // compute uniforms
-        let time = start_time.elapsed().as_secs_f32();
+    let mut vao = 0;
+    let mut vbo = 0;
+    let vs = compile_shader(VS_SRC, gl::VERTEX_SHADER);
+    let fs = compile_shader(FS_SRC, gl::FRAGMENT_SHADER);
+    let program = link_program(vs, fs);
 
-        // draw stuff
-        for stage in pipeline.stages.iter() {
-            let c0 = stage.channels[0].clone().map(|s| textures.get(&s)).flatten().unwrap_or(&dummy);
-            let c1 = stage.channels[1].clone().map(|s| textures.get(&s)).flatten().unwrap_or(&dummy);
-            let c2 = stage.channels[2].clone().map(|s| textures.get(&s)).flatten().unwrap_or(&dummy);
-            let c3 = stage.channels[3].clone().map(|s| textures.get(&s)).flatten().unwrap_or(&dummy);
+    unsafe {
+        gl::GenVertexArrays(1, &mut vao);
+        gl::GenBuffers(1, &mut vbo);
+    }
 
-            let uniforms = uniform! {
-                R: [width as f32, height as f32, time],
-                C0: c0, C1: c1, C2: c2, C3: c3
-            };
+    'running: loop {
+        use sdl2::event::Event;
+        use sdl2::keyboard::Keycode;
 
-            if let Some(tex) = stage.target.clone().map(|s| textures.get(&s)).flatten() {
-                let mut target = tex.as_surface();
-                target
-                    .draw(
-                        &vertex_buffer,
-                        &indices,
-                        &stage.prog,
-                        &uniforms,
-                        &Default::default(),
-                    )
-                    .unwrap();
-            } else {
-                let mut target = display.draw();
-                target
-                    .draw(
-                        &vertex_buffer,
-                        &indices,
-                        &stage.prog,
-                        &uniforms,
-                        &Default::default(),
-                    )
-                    .unwrap();
-                target.finish().unwrap();
+        for event in event_pump.poll_iter() {
+            imgui_sdl2.handle_event(&mut imgui, &event);
+            if imgui_sdl2.ignore_event(&event) {
+                continue;
+            }
+
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
             }
         }
 
-        // wait for next frame
-        let next_frame_time = Instant::now() + Duration::from_millis(5);
-        *control_flow = ControlFlow::WaitUntil(next_frame_time);
-    });
+        imgui_sdl2.prepare_frame(imgui.io_mut(), &window, &event_pump.mouse_state());
+
+        let now = Instant::now();
+        let delta = now - last_frame;
+        let delta_s = delta.as_secs() as f32 + delta.subsec_nanos() as f32 / 1_000_000_000.0;
+        last_frame = now;
+        imgui.io_mut().delta_time = delta_s;
+
+        let ui = imgui.frame();
+        ui.show_demo_window(&mut true); // Zhe magic
+
+        // compute uniforms
+        let (width, height) = window.size();
+        let time = start_time.elapsed().as_secs_f32();
+        //println!("{:?}", (width, height, time));
+
+        unsafe {
+            gl::ClearColor(0.0, 0.0, 0.0, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+
+            gl::BindVertexArray(vao);
+
+            // Create a Vertex Buffer Object and copy the vertex data to it
+            gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+            gl::BufferData(
+                gl::ARRAY_BUFFER,
+                (FULLSCREEN_RECT.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+                mem::transmute(&FULLSCREEN_RECT[0]),
+                gl::STATIC_DRAW,
+            );
+
+            // Use shader program
+            gl::UseProgram(program);
+            gl::BindFragDataLocation(program, 0, CString::new("out_color").unwrap().as_ptr());
+
+            // Specify the layout of the vertex data
+            let pos_attr =
+                gl::GetAttribLocation(program, CString::new("position").unwrap().as_ptr());
+
+            let r_loc = gl::GetUniformLocation(program, CString::new("R").unwrap().as_ptr());
+            gl::Uniform3f(r_loc, width as _, height as _, time);
+
+            let time_loc = gl::GetUniformLocation(program, CString::new("time").unwrap().as_ptr());
+            gl::Uniform1f(time_loc, time);
+
+            gl::EnableVertexAttribArray(pos_attr as GLuint);
+
+            gl::VertexAttribPointer(
+                pos_attr as GLuint,
+                2,
+                gl::FLOAT,
+                gl::FALSE as GLboolean,
+                0,
+                ptr::null(),
+            );
+
+            // Draw a triangle from the 3 vertices
+            gl::DrawArrays(gl::TRIANGLES, 0, 6);
+        }
+
+        imgui_sdl2.prepare_render(&ui, &window);
+        renderer.render(ui);
+
+        window.gl_swap_window();
+
+        std::thread::sleep(std::time::Duration::new(0, 1_000_000_000u32 / 60));
+    }
 }
